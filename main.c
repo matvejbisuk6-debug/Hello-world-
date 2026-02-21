d2a59bde985b5392e44a8bc5465241d6eea132c6
Matvey <matvejbisuk6@gmail.com>
2026-02-21 23:51:29 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,32 @@
+# include <stdio.h>
+int main(void)
+{
+    // Задача 1
+
+    //printf("Hello world\n");
+    //printf("Hello C\n");
+
+    // Задача 2
+
+    //int = n;
+    //char = c;
+    //float = f;
+    //double = d;
+
+    // Задача 3
+
+    char sym = 'F';
+    int num = 10;
+    float f = 3.14;
+    double d = 3.14159;
+
+    // Задача 4
+
+    printf("%d\n", num);
+    printf("%c\n", sym);
+    printf("%f\n", f);
+    printf("%lf\n", d);
+
+    return 0;
+}
+
