23b13ffd9100235238fe16590d65509d14cb4698
Matvey <matvejbisuk6@gmail.com>
2026-04-05 22:29:54 +0300

    Add files via upload
    The second topic is where i solved problems and learned the types of variables in the C language and arithmetic operations

---
+++ Zadachi_2.c
@@ -0,0 +1,58 @@
+# include <stdio.h>
+
+int main(){
+
+	//Задача 1
+	//int x = 5;
+	//int y = 7;
+	//int res;
+
+	//res = x + y;
+
+	//printf("Result = %d \n", res);
+
+	//Задачи 2
+	//int a = 10;
+	//int b = 4;
+	//int c;
+	
+	//c = a / b;
+	//printf("%d / %d = %d \n", a, b, c);
+	
+	//Задача 3
+	//float a = 10;
+	//float b = 4;
+	//float c;
+	
+	//c = a / b;
+	//printf("%f / %f = %f \n", a, b, c);
+	
+	//задача 4
+	//int a = 10;
+	//int b = 5;
+	//int c;
+	//int average;
+	
+	//c = a + b;
+	//average = c / 2;
+	//printf("%d + %d = %d \n", a, b, c);
+	//printf("%d / 2 = %d \n", c, average);
+	
+	//Задача 5
+	//int a = 10;
+	//int b = 3;
+	//int c;
+	
+	//c = a % b;
+	//printf("%d %% %d = %d \n", a, b, c);
+	
+	//Задача 6
+	//float a = 10;
+	//float b = 3;
+	//float c;
+	
+	//c = a / b;
+	//printf("Result: %.3f \n", c);
+	
+	return 0;
+}
