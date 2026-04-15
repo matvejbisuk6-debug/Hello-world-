a4974783ab4e61b7e37c487e9c791c64eba1951c
Matvey <matvejbisuk6@gmail.com>
2026-04-15 22:58:22 +0300

    Add files via upload
    I continued to dive into the C language, specifically into loops

---
+++ Zadachi_8.c
@@ -0,0 +1,63 @@
+# include <stdio.h>
+
+int main() {
+	//Задача 1
+	//int n;
+	
+	//printf("Enter number: \n");
+	//scanf("%d", &n);
+	
+	//for(int i = 1; i <= n; n--) {
+	//	printf("%d \n", n);
+	//}
+	
+	//Задача 2
+	//int n;
+	
+	//printf("Enter number: \n");
+	//scanf("%d", &n);
+	
+	//for(int i = 2; i <= n; i++) {
+	//	if(i % 2 == 0) {
+	//		printf("%d \n", i);
+	//	}
+	//}
+	
+	//Задача 3
+	//int n;
+	
+	//printf("Enter number: \n");
+	//scanf("%d", &n);
+	
+	//for(int i = 1; i < n; i++) {
+	//	if(i % 2 != 0) {
+	//		printf("%d \n", i);
+	//	}
+	//}
+	
+	//Задача 4
+	//int a, b;
+	//int sum = 0;
+	
+	//printf("Enter numbers: \n");
+	//scanf("%d %d", &a, &b);
+	
+	//for(int i = a; i <= b; i++) {
+	//	sum += i;
+	//}
+	//printf("Total amount from %d to %d = %d \n", a, b, sum);
+	
+	//Задача 5
+	//int n;
+	//long fact = 1;
+	
+	//printf("Enter number: \n");
+	//scanf("%d", &n);
+	
+	//for(int i = 1; i <= n; i++) {
+	//	fact *= i;
+	//}
+	//printf("Factorial of the number %d = %d \n", n, fact);
+	
+	return 0;
+}
