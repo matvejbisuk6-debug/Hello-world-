afb94ee532d8f6c226eabfc60697128af77f2169
Matvey <matvejbisuk6@gmail.com>
2026-04-20 22:43:24 +0300

    Add files via upload

---
+++ Zadachi_11.c
@@ -0,0 +1,54 @@
+# include <stdio.h>
+
+int main() {
+	//Задача 1
+	//int n;
+	
+	//while(1) {
+	//	printf("Enter number: \n");
+	//	scanf("%d", &n);
+		
+	//	if(n > 0 || n < 0) {
+	//		n += n;
+	//	} else if(n == 0) {
+	//		printf("The cycle is over");
+	//		break;
+	//	}
+	//	printf("Sum of all numbers: %d \n", n);
+	//}
+	
+	//Задача 2
+	//int n;
+	//int count = 0;
+	//int sum = 0;
+
+	//while(count < 6) {
+		
+		//printf("Enter the positive number%d: \n", count);
+	   // scanf("%d", &n);
+	
+		//if(n <= 0) {
+		//	continue;
+		//} 
+		//	count++;
+		//	sum += n;
+			
+		//}
+	//printf("The sum of 6 positive numbers: %d \n", sum);
+	
+	//Задача 3
+	//int n;
+	//int i = 1;
+	//int res;
+	
+	//printf("Enter number: \n");
+	//scanf("%d", &n);
+	
+	//while(i <= 10) {
+	//	res = n * i;
+	//	printf("%d * %d = %d \n", n, i, res);
+	//	i++;
+	//}
+	
+	return 0;
+}
